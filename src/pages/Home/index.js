4d0e2f22b2bc1e8229f937492b4814166358171e
Taras Bobrovytsky <t.bobrovytsky@gmail.com>
2021-11-19 17:30:40 -0600

    Update home page contents

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -56,47 +56,19 @@ const Home = (props) => {
             <ul>
               <li>
                 <Typography align="left" variant="body1" gutterBottom>
-                  Minting starts 11/11/2021 at 7:00pm EST on{" "}
-                  <Link
-                    target="_blank"
-                    href="https://arbitrum.io"
-                    style={{ cursor: "pointer" }}
-                  >
-                    Arbitrum
-                  </Link>
+                  100% of the ΞTH spent on minting goes back to the minters through
+                  an&nbsp;
+                  <Link href="/redeem" style={{ cursor: "pointer" }}>
+                    interesting mechanism
+                  </Link>{" "}
                   .
                 </Typography>
               </li>
               <li>
                 <Typography align="left" variant="body1" gutterBottom>
-                  Price starts at 0.001 ΞTH and increases after each mint.
+                  Trade your NFTs on the built in 0.00% fee marketplace.
                 </Typography>
               </li>
-              <li>
-                <Typography align="left" variant="body1" gutterBottom>
-                  100% of the ΞTH spent on minting goes back to the minters.
-                  Check the&nbsp;
-                  <Link href="/redeem" style={{ cursor: "pointer" }}>
-                    redeem
-                  </Link>{" "}
-                  page.
-                </Typography>
-              </li>
-              <li>
-                <Typography align="left" variant="body1" gutterBottom>
-                  Trade your NFTs on the 0% fee marketplace after November 11.
-                </Typography>
-              </li>
-              {/* <li>
-              <Typography align="left" variant="body1" gutterBottom>
-                Those who mint during the first 48 hours may be eligible to
-                receive a prize, check out the&nbsp;
-                <Link href="/giveaway" style={{ cursor: "pointer" }}>
-                  giveaway
-                </Link>
-                .
-              </Typography>
-            </li> */}
             </ul>
           </Box>
           <Box mt={3} className={classes.centerMobile}>
